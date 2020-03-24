a6ffac0e0c50ef2f4f59aacccd5f5fc0b55b9647
Brad Peabody <brad.peabody@broadcom.com>
2020-03-24 10:04:26 -0400

    Updates

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -7,6 +7,7 @@ echo deltarpm=0 >> /etc/yum.conf
 
 # Create a user on guest for the okta username.  This makes the signing
 # script happy because it checks to make sure the user exists on the system.
+echo "Creating user $USER"
 if ! id -u $USER; then
     adduser -rm $USER
 fi
@@ -16,7 +17,11 @@ echo "Archiving tools on $VDI (this will take a while)"
 # SSH to VDI and tar/gzip the tools required
 ssh -o StrictHostKeyChecking=no \
     -i /home/vagrant/.ssh/id_rsa \
-    $USER@$VDI 'tar -czf /tmp/tools.tgz /projects/ccxsw_tools/mentor_graphics/mgc-2018.070 /projects/ccxsw_tools/sbl_tools'
+    $USER@$VDI \
+    'tar -czf /tmp/tools.tgz \
+        /projects/ccxsw_tools/mentor_graphics/mgc-2018.070 \
+        /projects/ccxsw_tools/sbl_tools \
+        /projects/ccxsw_tools/contrib'
 
 # SCP the tools
 echo "Copying tools from $VDI (this will take a while)"
@@ -35,20 +40,20 @@ ssh -o StrictHostKeyChecking=no \
     -i /home/vagrant/.ssh/id_rsa \
     $USER@$VDI 'rm /tmp/tools.tgz'
 
-#cd /
-#tar -xzf /vagrant/mentor_graphics.tgz
-#tar -xzf /vagrant/sbl_tools.tgz
-#chown --recursive vagrant:vagrant /projects
-
 # Update all packages
+echo "Updating all packages"
 yum update -y
 
 # Install newer version of git - v2.16.6.  Centos distro comes with v1.8
+echo "Installing newer version of git"
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm \
 
-# Install packages needed for build
+# Install additional packages
 # gcc/++ for building bnxPkgUtil
+echo "Installing new packages"
 yum install -y \
+    zsh \
+    vim \
     cmake3 \
     gcc gcc-c++ \
     git2u \
@@ -59,30 +64,35 @@ yum install -y \
     perl-WWW-Mechanize \
     sudo \
     tcl-devel \
-    npm \
-
-# Install some optional packages
-yum install -y \
-    zsh \
-    vim
-
-# Upgrade zsh to 5.1, so powerlevel10k theme will work
-yum install -y http://mirror.ghettoforge.org/distributions/gf/el/7/plus/x86_64/zsh-5.1-1.gf.el7.x86_64.rpm
+    npm
 
 yum clean all
 
-# Get powerlevel10k source from github
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/vagrant/powerlevel10k
-
 # Build needs cmake3 but calls cmake
+echo "Linking /usr/vin/cmake3 to /usr/bin/cmake"
 ln -sf /usr/bin/cmake3 /usr/bin/cmake \
 
 # Signing scripts looks for perl in /usr/local/bin
+echo "Linking /usr/bin/perl ==> /usr/local/bin"
 ln -sf /usr/bin/perl /usr/local/bin \
 
+# Set netxtreme repo push origin to Gerrit and install client side git hooks
+echo "Initializing /git/netxtreme repo (push to Gerrit, client hooks)"
+cd /git/netxtreme
+runuser -l vagrant -c '/projects/ccxsw_tools/contrib/git/ccxswinit.pl'
+
+# zsh is used by default.  If you want to stick with bash, comment out these commands.
+# You'll need to build your own .bashrc
+# Upgrade zsh to 5.1, so powerlevel10k theme will work
+echo "Upgrading zsh and installing Powerlevel10k theme"
+yum install -y http://mirror.ghettoforge.org/distributions/gf/el/7/plus/x86_64/zsh-5.1-1.gf.el7.x86_64.rpm
 # Set shell to zsh for default vagrant user
 usermod --shell /bin/zsh vagrant
+# Get powerlevel10k source from github
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/vagrant/powerlevel10k
 
 # Add USER to .zshrc file.  Signing script uses this variable.
+echo "Adding USER=$USER to .zshrc"
 echo export USER=$USER >> /home/vagrant/.zshrc
 
+echo "Bootstrap Complete!"
